6760a744a047f580baf64d7f4a41f1178641a001
Paras Chodavadiya <paras.chodavadiya@gmail.com>
2017-12-01 22:27:11 +0530

    First Commit

--- app/src/main/java/com/iblinfotech/foodierecipe/utils/AppConfig.java
+++ app/src/main/java/com/iblinfotech/foodierecipe/utils/AppConfig.java
@@ -6,7 +6,6 @@ public class AppConfig {
     public static final String BASE_API_URL = SERVER_URL + "RestAPI/";
     public static final String IMAGE_URL = SERVER_URL;
 
-
     public static final String RESPONSE_MESSAGE = "ResponseMessage";
     public static final String RESPONSE_CODE = "ResponseCode";
     public static final String STATUS_CODE = "Statuscode";
